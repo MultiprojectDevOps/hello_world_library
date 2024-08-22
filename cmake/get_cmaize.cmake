66aca0cbd30d27519ef780b7e633a662ed03f5ea
ryan <ryanmrichard1@gmail.com>
2024-08-22 09:52:26 -0500

    fix licenses

--- cmake/get_cmaize.cmake
+++ cmake/get_cmaize.cmake
@@ -1,4 +1,4 @@
-# Copyright 2024 Multiproject DevOps Team
+# Copyright 2024 Multi-Project DevOps
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
